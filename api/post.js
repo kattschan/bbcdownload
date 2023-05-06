8a6f8e63ab285ce8d707863965e825ff23ac8f14
Kat Tschan <kat@kattschan.co.uk>
2023-05-06 15:17:32 -0400

    Fix post because I did it on another machine

--- api/post.js
+++ api/post.js
@@ -6,10 +6,16 @@ module.exports = function(req, res, next) {
     const time = req.body.time;
     const description = req.body.description;
     console.log(req);
+    // Check for errors(i.e. time and description are empty)
+    if (time === '' || description === '' || time === undefined || description === undefined || time === null || description === null || time === 'undefined' || description === 'undefined') {
+        // Return 400 error
+        res.status(400).send('Error: Time and description cannot be empty');
+    } else {
     // Insert the time and description into the bloopers table
     const insert = db.prepare('INSERT INTO bloopers (time, description) VALUES (?, ?)');
     insert.run(time, description);
     // alert the user that the blooper was added
     res.send('alert("Blooper added!")')
     console.log('hi')
+    }
 }
